3df8afda1900b629d8071c3fecc261b5942fae2a
hathuytien <thuytienha127@gmail.com>
2022-05-28 15:17:55 +0700

    a

---
+++ assets/About.8769977c.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.eb675ab0.js";import{o,a as s,d as a}from"./vendor.75ab2dbf.js";const t={},c={class:"about"},n=a("h1",null,"This is an about pagea",-1),r=[n];function _(d,i){return o(),s("div",c,r)}var l=e(t,[["render",_]]);export{l as default};
